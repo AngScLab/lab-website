909911c84bb17b4ef116154d597d3208abf7ac85
khaichung2807 <104648642+khaichung2807@users.noreply.github.com>
2026-04-21 22:04:03 +0000

    Deploying to gh-pages from @ AngScLab/lab-website@5670673e7691d93fa74efe4125b1a5927eab5bc7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,7 +53,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "Team members",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
