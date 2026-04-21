1ec4d9fa3db0c99821537ddec5e787e192b354b1
khaichung2807 <104648642+khaichung2807@users.noreply.github.com>
2026-04-21 21:47:29 +0000

    Deploying to gh-pages from @ AngScLab/lab-website@e9e451dae68460f1628f018aeeeeec780bbf055a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "We study how genomes encode biological diversity and what happens when that diversity meets a changing world.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
